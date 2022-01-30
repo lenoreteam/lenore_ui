080bf93ea9d5c264ac0439f6ffd02845ad8d5a2a
Arash Mohammadi <arash13131313@gmail.com>
2022-01-30 16:27:14 +0330

    add color,textAlignment, boxShadow to LenoreButton

--- lib/src/widgets/LenoreButton.dart
+++ lib/src/widgets/LenoreButton.dart
@@ -8,14 +8,20 @@ class LenoreButton extends StatelessWidget {
   final Function()? onPressed;
   final IconData? icon;
   final bool isLoading;
+  final Alignment textAlignment;
+  final Color? backgroundColor;
+  final List<BoxShadow>? boxShadow;
 
-  LenoreButton(
-      {Key? key,
-      required this.caption,
-      this.onPressed,
-      this.icon,
-      this.isLoading = false})
-      : super(key: key);
+  LenoreButton({
+    Key? key,
+    required this.caption,
+    this.onPressed,
+    this.icon,
+    this.isLoading = false,
+    this.textAlignment = Alignment.center,
+    this.backgroundColor,
+    this.boxShadow,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -23,15 +29,17 @@ class LenoreButton extends StatelessWidget {
       onTap: this.onPressed,
       child: Container(
         decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
+            color: backgroundColor ?? Theme.of(context).primaryColor,
             borderRadius: BorderRadius.circular(10),
-            boxShadow: [
-              BoxShadow(
-                  color: Theme.of(context).shadowColor,
-                  blurRadius: 5,
-                  spreadRadius: 1)
-            ]),
+            boxShadow: boxShadow ??
+                [
+                  BoxShadow(
+                      color: Theme.of(context).shadowColor,
+                      blurRadius: 5,
+                      spreadRadius: 1)
+                ]),
         padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+        alignment: textAlignment,
         child: this.isLoading
             ? Container(
                 child: LenoreLoading(),
